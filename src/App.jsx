f4719996ce8bfe021178443c15daa23a01d4b8b4
josuafrino <josuafrino@gmail.com>
2024-06-05 23:07:17 +0700

    Quill JS implementation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HtmlEditor from "./components/HtmlEditor/HtmlEditor";
+
+function App() {
+  return (
+    <>
+      <div>
+        <HtmlEditor></HtmlEditor>
+      </div>
+    </>
+  );
+}
+
+export default App;
